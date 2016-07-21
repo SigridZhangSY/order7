2a474ca94c852a365c4978dc4a0c9d2f7aa7a13c
SigridZhangSY <sigridzhang0603@163.com>
2016-07-21 19:09:14 +0800

    return 201 when post user with specified parameter

--- src/main/java/com/thoughtworks/ketsu/web/UsersRespurce.java
+++ src/main/java/com/thoughtworks/ketsu/web/UsersRespurce.java
@@ -1,18 +1,23 @@
 package com.thoughtworks.ketsu.web;
 
 import com.thoughtworks.ketsu.domain.user.User;
+import com.thoughtworks.ketsu.domain.user.UserRepository;
 import com.thoughtworks.ketsu.web.jersey.Routes;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import java.util.Map;
 
 @Path("users")
 public class UsersRespurce {
 
     @POST
-    public Response postUsers(@Context Routes routes){
-        return Response.created(routes.userUri(new User(1))).build();
+    public Response postUsers(Map<String, Object> info,
+                              @Context Routes routes,
+                              @Context UserRepository userRepository){
+        User user = userRepository.postUser(info).get();
+        return Response.created(routes.userUri(user)).build();
     }
 }
--- src/test/java/com/thoughtworks/ketsu/web/UsersResourceTest.java
+++ src/test/java/com/thoughtworks/ketsu/web/UsersResourceTest.java
@@ -2,6 +2,7 @@ package com.thoughtworks.ketsu.web;
 
 import com.thoughtworks.ketsu.support.ApiSupport;
 import com.thoughtworks.ketsu.support.ApiTestRunner;
+import com.thoughtworks.ketsu.support.TestHelper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,8 +17,8 @@ import static org.junit.Assert.assertThat;
 public class UsersResourceTest extends ApiSupport{
 
     @Test
-    public void should_return_uri_when_post_uses(){
-        Response post = post("users", new HashMap<String, Object>());
+    public void should_return_201_when_post_uses_with_specified_parameter(){
+        Response post = post("users", TestHelper.userMap("name"));
         assertThat(post.getStatus(), is(201));
         assertThat(Pattern.matches(".*/users/[0-9-]*",post.getLocation().toString()), is(true));
     }
